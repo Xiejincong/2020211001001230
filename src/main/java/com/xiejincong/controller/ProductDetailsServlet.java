6a793b558f05e79b8ee719bd11c0aea522a81a3e
Xiejincong <101031624+Xiejincong@users.noreply.github.com>
2022-05-31 16:13:24 +0800

    Update ProductDetailsServlet.java

--- src/main/java/com/xiejincong/controller/ProductDetailsServlet.java
+++ src/main/java/com/xiejincong/controller/ProductDetailsServlet.java
@@ -1,4 +1,8 @@
 package com.xiejincong.controller;
+import com.xiejincong.dao.ProductDao;
+import com.xiejincong.model.Category;
+import com.xiejincong.model.Product;
+
 
 import javax.servlet.*;
 import javax.servlet.http.*;
