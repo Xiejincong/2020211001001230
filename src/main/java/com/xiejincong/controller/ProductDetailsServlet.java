2a8fcbf0dda01c4b3bb9d7710e9f3279a3aa41a9
Xiejincong <101031624+xiejincong@users.noreply.github.com>
2022-05-31 14:48:16 +0800

    week 12 commit

---
+++ src/main/java/com/xiejincong/controller/ProductDetailsServlet.java
@@ -0,0 +1,51 @@
+package com.xiejincong.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(name = "ProductDetailsServlet", value = "/productDetails")
+public class ProductDetailsServlet extends HttpServlet {
+    Connection dbConn=null;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        dbConn=(Connection) getServletContext().getAttribute("dbConn");
+    }
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id=request.getParameter("id")!=null?Integer.parseInt(request.getParameter("id")):0;
+        ProductDao productDao=new ProductDao();
+        if(id==0){
+            return;
+        }
+        try {
+            List<Category> categoryList=Category.findAllCategory(dbConn);
+            request.setAttribute("categoryList",categoryList);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Product product=productDao.findById(id,dbConn);
+        request.setAttribute("p",product);
+        String path="/WEB-INF/views/productDetails.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/com/xiejincong/controller/ShopServlet.java
@@ -0,0 +1,60 @@
+package com.xiejincong.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "ShopServlet", value = "/shop")
+public class ShopServlet extends HttpServlet {
+    Connection dbConn=null;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        dbConn=(Connection) getServletContext().getAttribute("dbConn");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Category category=new Category();
+        List<Category> categoryList= null;
+        try {
+            categoryList = category.findAllCategory(dbConn);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        request.setAttribute("categoryList",categoryList);
+
+        ProductDao productDao=new ProductDao();
+        List<Product> productList=null;
+        try {
+            if(request.getParameter("categoryId")==null){
+                productList=productDao.findAll(dbConn);
+            }else {
+                int categoryId=Integer.parseInt(request.getParameter("categoryId"));
+                productList=productDao.findByCategoryId(categoryId,dbConn);
+            }
+        }catch (Exception e){
+            System.out.println("oh No");
+        }
+        request.setAttribute("productList",productList);
+        String path="/WEB-INF/views/shop.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
